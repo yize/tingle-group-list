ccab7e0150518870e94d55b427bfddc1d0f56d49
拂山 <jiasong.js@alibaba-inc.com>
2015-06-10 12:57:58 +0800

    update

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,28 +2,32 @@ var webpack = require('webpack');
 module.exports = {
     cache: false,
     entry: {
-        demo: './demo/index' // 自定义位置
+        demo: './demo/index'
     },
     output: {
-        // 这个path配置和pipe.dest()冲突
-        // 所以如果使用"gulp watch" 此行要注释掉
-        // 如果使用"webpack --watch" 此行要打开
-        // path: './dist', 
+        path: './dist', 
         filename: "[name].js",
         sourceMapFilename: "[name].js.map"
     },
     devtool: '#source-map', // 这个配置要和output.sourceMapFilename一起使用
     module: {
         loaders: [
-            {test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'}
+            {test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader?stage=1'}
         ]
     },
     resolve: {
         alias: {
-            'tingle-text-field': __dirname + '/tingle/tingle-text-field/src' // 自定义位置
+            'tingle-text-field': __dirname + '/tingle/tingle-text-field/src' // 自定义别名
         }
     },
     externals: {
         react: 'var React' // 相当于把全局的React作为模块的返回 module.exports = React;
-    }
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+          __LOCAL__: true, // 本地环境
+          __DEV__:   true, // 日常环境
+          __PRO__:   false // 生产环境
+        })
+    ]
 };
